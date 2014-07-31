117ec6529e4259681b246624367e1cb7d049d373
Kevin Mershon <kevin@slixbits.com>
2014-07-31 16:43:19 -0700

    Stringify the receivers to be compatible with old browsers

--- html5/js/main.js
+++ html5/js/main.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
             data: {
                 subject: $("#subject").val(),
                 body: $("#body").val(),
-                receivers: _.union(
+                receivers: JSON.stringify(_.union(
                     _.map($("#to").val().split(","), function(it) {
                         return { type: "to", address: it.trim()};
                     }),
                     _.map($("#cc").val().split(","), function(it) {
                         return { type: "cc", address: it.trim()};
                     })
-                )
+                ))
             },
             success: function(response, status, xhr) {
                 console.log(response.responseText);
