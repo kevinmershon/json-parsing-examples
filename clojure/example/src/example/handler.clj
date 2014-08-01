b3ddfbe18daf9fc6aa203cbf26c10e4733ee94f8
Kevin Mershon <kevin@slixbits.com>
2014-08-01 07:30:45 -0700

    Clojure example index now automatically loads

--- clojure/example/src/example/handler.clj
+++ clojure/example/src/example/handler.clj
@@ -2,9 +2,12 @@
   (:use [example.sender])
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :refer [resource-response response]]))
 
 (defroutes app-routes
+  (GET "/" []
+       (resource-response "index.html" {:root "public"}))
   (POST "/send" {session :session
                  params :params}
         (let [receivers (:receivers params)
