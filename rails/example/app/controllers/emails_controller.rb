f97ecf611c3340d4b501a9c9976f2c2af9dcf2a3
Brent Houghton <brent@slixbits.com>
2014-07-31 23:18:36 -0700

    Fill in controller logic in the Rails example.

--- rails/example/app/controllers/emails_controller.rb
+++ rails/example/app/controllers/emails_controller.rb
@@ -1,10 +1,44 @@
 class EmailsController < ApplicationController
   def new
+    @email = Email.new
   end
 
   def create
+
+    @email = Email.new(email_create_params)
+
+  	# receivers contains both the to and cc values which have to be separated
+    to_values = []
+    cc_values = []
+  	params.receivers.each do |element|
+      if element.type == "to"
+        to_values << element.address
+      elsif element.type == "cc"
+        cc_values << element.address
+      end
+  	end
+  	@email.to = to_values.join ", "
+  	@email.cc = cc_values.join ", "
+    @email.save
+
+    redirect_to email_path(@email)
   end
 
   def show
+    if Email.exists?(:id => params[:id])
+      @email = Email.find(params[:id])
+    else
+      flash[:error] = "That email does not exist."
+      redirect_to root_url
+    end
   end
+
+  private
+
+    def email_create_params
+      params.permit(
+        :subject,
+        :body
+      )
+    end
 end
